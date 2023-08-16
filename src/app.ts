8fba565a91abc1f82d46a991190c366b2fd19937
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-16 11:50:41 +0100

    feat: Refactor of API

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
 import express from "express";
+import { router } from "./routes/routes";
 export const app = express();
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 app.get("/coffeelover", (req, res) => res.send("I like Coffee!"));
 app.use(express.json());
-app.get("/coffee", (req, res) =>
-  res.json({
-    drinkType: "Coffee",
-    name: "Latte",
-  })
-);
+app.use("/", router);
