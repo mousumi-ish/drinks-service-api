aed85b4864c711206b5980590080dd7afbba453a
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-16 15:41:17 +0100

    feat: Implement tealover endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,4 @@ app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
 app.get("/coffeelover", (req, res) => res.send("I like Coffee!"));
 app.use(express.json());
 app.use("/", router);
+app.get("/tealover", (req, res) => res.send("I like Tea!"));
